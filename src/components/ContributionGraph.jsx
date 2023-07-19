74c7d2dd9455184ee1ab73115badd81cb6ef8248
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2023-07-19 19:45:29 +0600

    fetching data from server

--- src/components/ContributionGraph.jsx
+++ src/components/ContributionGraph.jsx
@@ -1,14 +1,31 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { getDates, getMonths } from "../utils/date";
 import Day from "./Day";
 import Levels from "./Levels";
 import WeekDays from "./WeekDays";
+import axios from "axios";
+
+const API = "https://dpg.gg/test/calendar.json";
+
+const dates = getDates();
+const months = getMonths();
 
 const ContributionGraph = () => {
-	const dates = useMemo(getDates, []);
-	const months = getMonths();
-
 	const [selectedDay, setSelectedDay] = useState(null);
+	const [contributions, setContributions] = useState({});
+
+	async function getContributions() {
+		try {
+			const { data } = await axios.get(API);
+			setContributions(data);
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	useEffect(() => {
+		getContributions();
+	}, []);
 
 	return (
 		<div>
@@ -26,6 +43,7 @@ const ContributionGraph = () => {
 
 					{dates.map((day, i) => (
 						<Day
+							contributions={contributions}
 							selectedDay={selectedDay}
 							setSelectedDay={setSelectedDay}
 							key={i}
